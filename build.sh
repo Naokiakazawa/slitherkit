e5cf114002c273a596b93655c6fc411410535364
Naokiakazawa <naoki9akazawa@gmail.com>
2022-07-10 12:39:47 +0900

    add a build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Solidity version: 0.8.4 - 0.8.15
+# Slither version: v0.8.3
+
+echo "check environment"
+#docker images slitherkit | grep 
+
+
+
+echo "start building"
+
+for ver in {4..15}
+do
+    version="0.8.$ver"
+    echo "===> Build a docker image of $version"
+    docker build -t slitherkit --build-arg SOLC=$version .
+
+    echo "===> Push a docker image of $version"
+    docker tag $(docker images slitherkit -q) naokiakazawa/slitherkit:$version
+    docker images | grep slitherkit
+    docker push naokiakazawa/slitherkit:$version
+
+    echo "===> Clean up a docker image of $version"
+    docker image rm -f $(docker images slitherkit -q)
+done
+
+docker builder prune -y
+echo "finish!"
